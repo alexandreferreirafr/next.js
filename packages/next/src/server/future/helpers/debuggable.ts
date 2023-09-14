1f30badbfd795ab5dc471efde0b34c6723672da5
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-09-14 01:07:58 -0600

    refactor: refactored route definition impl

---
+++ packages/next/src/server/future/helpers/debuggable.ts
@@ -0,0 +1,16 @@
+import debug from 'next/dist/compiled/debug'
+
+/**
+ * Adds debug log functionality to a class. Any class that extends this class
+ * will have access to the `debug` method, which can be used to log debug
+ * messages with the `next:debug` namespace and the class name.
+ */
+export class Debuggable {
+  protected readonly debug: debug.Debugger
+
+  constructor() {
+    this.debug = debug('next:debug').extend(
+      this.constructor.name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+    )
+  }
+}
